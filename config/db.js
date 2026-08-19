29e06ddd39c4e68c83f4043b755a659e6d735c02
meghasree-09 <saimeghanasree09@gmail.com>
2026-08-19 21:28:55 +0530

    update database configurations

--- config/db.js
+++ config/db.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const dns = require("node:dns");
+require("dotenv").config();
+
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
@@ -6,7 +11,8 @@ const connectDB = async () => {
 
     console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error("MongoDB Connection Error:", error.message);
+    console.error("Database connection failed");
+    console.error(error);
     process.exit(1);
   }
 };
