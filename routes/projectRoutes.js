d0ec5360efca7fa7c2cbe0da8001f14282bc57d3
meghasree-09 <saimeghanasree09@gmail.com>
2026-08-23 23:54:38 +0530

    update project routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -3,15 +3,25 @@ const express = require("express");
 const {
   getProjects,
   getProject,
-  createProject
+  createProject,
+  updateProject,
+  deleteProject
 } = require("../controllers/projectController");
+
+const protectAdmin = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+// Public routes
 router.get("/", getProjects);
 
 router.get("/:id", getProject);
 
-router.post("/", createProject);
+// Admin routes
+router.post("/", protectAdmin, createProject);
+
+router.put("/:id", protectAdmin, updateProject);
+
+router.delete("/:id", protectAdmin, deleteProject);
 
 module.exports = router;
